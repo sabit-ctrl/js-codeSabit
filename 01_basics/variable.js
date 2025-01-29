62334658d30b8d778bfcbcc8272db2057f37da02
sabit-ctrl <jayedibnisabit@gmail.com>
2025-01-29 13:04:21 +0600

    variable.js

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 1234;
+let accountEmail ="jayedibnisabit@gmail.com";
+var accountPassword="12121";
+accountCity = "jaipur";
+let accountState;
+//accountId= 1243;   **Not Allowed to re declare a Const Variable **
+
+accountEmail = "jerseyarena2727@gmail.com"
+accountPassword = 1111
+accountCity = "Dhaka";
+/*
+prefer not to use var
+because of issue in block scope and in funtional scope 
+*/
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
