73c5d47c6dd6aa83891b7fc643791c11ca28e7d7
sabit-ctrl <jayedibnisabit@gmail.com>
2025-01-29 07:42:03 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,22 @@
+let score=99
+let newScore="123"
+
+console.log(typeof score)
+console.log(typeof newScore)
+
+let scoreInNumber=Number(score)
+let newScoreInNumber=Number(score)
+
+console.log(typeof scoreInNumber)
+console.log(typeof newScoreInNumber)
+
+/* When Javascript Fails to give correct ans  */
+
+let run = "123abc"
+
+let runInNumber = Number(run)
+
+console.log(runInNumber )//Output:NaN
+console.log(typeof runInNumber )//Output:number
+
+let 
