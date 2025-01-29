8b7b059cda0b96f6367d99078f5dd5154193be71
sabit-ctrl <jayedibnisabit@gmail.com>
2025-01-29 07:20:10 +0000

    dataTypes

---
+++ 01_basics/dataType.js
@@ -0,0 +1,19 @@
+// Number=>1234
+//bigInt
+//String=>Charecter
+//boolean=>True/False
+//Undefined
+//Null
+//Symbol
+//Object
+
+let Roll = 124;
+let Name = "Sabit";
+let Sabit;
+let Abid = null
+
+
+console.log(typeof(Roll))
+console.log(typeof(Name))
+console.log(typeof(Sabit))
+console.log(typeof(Abid))//object
