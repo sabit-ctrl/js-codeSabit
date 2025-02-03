bd729ac44be69db44df705ae21715ef6a0fc868b
sabit-ctrl <jayedibnisabit@gmail.com>
2025-02-03 00:16:11 -0800

    Update conversion.js

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,14 +1,14 @@
-let score=99
-let newScore="123"
+let score=99     
+let newScore="123"  
 
-console.log(typeof score)
-console.log(typeof newScore)
+console.log(typeof score)         // number
+console.log(typeof newScore)      //string
 
-let scoreInNumber=Number(score)
+let scoreInNumber=Number(score) 
 let newScoreInNumber=Number(score)
 
-console.log(typeof scoreInNumber)
-console.log(typeof newScoreInNumber)
+console.log(typeof scoreInNumber)             //number   
+console.log(typeof newScoreInNumber)        //number
 
 /* When Javascript Fails to give correct ans  */
 
@@ -23,9 +23,9 @@ let some = null
 
 let someInNumber = Number(some) 
 
-console.log(typeof some)//that shows object. what we discussed before 
-console.log(typeof someInNumber)//that shows number but it is undefined to number
-console.log(someInNumber)//that shows 0 what is not
+console.log(typeof some)                //that shows object. what we discussed before 
+console.log(typeof someInNumber)        //that shows number but it is undefined to number
+console.log(someInNumber)           //that shows 0 what is not
 
 //"33"=>33
 //"33abc"=>NaN
